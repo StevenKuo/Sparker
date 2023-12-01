c64321a7d9c99dc8d318e2ae728e44453ce51ec1
StevenKuo <steven_581@hotmail.com>
2023-12-01 10:50:51 +0800

    Replace API domain

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import { Layout, Menu, Button, Modal, Input, Skeleton, message } from 'antd';
-import './index.scss';
-import 'AppRoot/App.less';
-import { useAuth } from '../../auth/context';
-import logo from '../../assets/logo.png';
-import axios from 'axios';
-import { refreshAppList } from './duck';
-import AppLayout from '../applayout';
-import AppModal from '../appmodal';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { Layout, Menu, Button, Modal, Input, Skeleton, message } from "antd";
+import "./index.scss";
+import "AppRoot/App.less";
+import { useAuth } from "../../auth/context";
+import logo from "../../assets/logo.png";
+import axios from "axios";
+import { refreshAppList } from "./duck";
+import AppLayout from "../applayout";
+import AppModal from "../appmodal";
 
 function Home() {
   const apps = useSelector(({ home }) => home.apps);
@@ -23,108 +23,123 @@ function Home() {
   const { t } = useTranslation();
 
   const getApps = () => {
-    setLoading(true)
-    axios.get('https://sheltered-garden-95685.herokuapp.com/apps')
-    .then((response) => {
-      setLoading(false)
-      dispatch(refreshAppList(response.data.apps))
-    }).catch((error) => {
-      setLoading(false)
-      if (error.response.status === 401) {
-        auth.signout(() => {})
-      }
-    })
-  }
+    setLoading(true);
+    axios
+      .get("https://sparker-api.onrender.com/apps")
+      .then((response) => {
+        setLoading(false);
+        dispatch(refreshAppList(response.data.apps));
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response.status === 401) {
+          auth.signout(() => {});
+        }
+      });
+  };
   const createApp = (name) => {
-    axios.post('https://sheltered-garden-95685.herokuapp.com/apps', {name})
-    .then(function (response) {
-      setCreateModal(false)
-      getApps()
-    })
-    .catch(function (error) {
-      auth.signout(() => {})
-    });
-  }
+    axios
+      .post("https://sparker-api.onrender.com/apps", { name })
+      .then(function (response) {
+        setCreateModal(false);
+        getApps();
+      })
+      .catch(function (error) {
+        auth.signout(() => {});
+      });
+  };
 
   const publishLayout = (layout) => {
-    const publishing = message.loading(t('publishing'), 0)
-    axios.post('https://sheltered-garden-95685.herokuapp.com/layout', {id: app.id, layout: JSON.stringify(layout)})
-    .then(function (response) {
-      publishing()
-      message.success(t('publish_success'), 2.5)
-      setApp({
-        ...app,
-        layout
+    const publishing = message.loading(t("publishing"), 0);
+    axios
+      .post("https://sparker-api.onrender.com/layout", {
+        id: app.id,
+        layout: JSON.stringify(layout),
       })
-    })
-    .catch(function (error) {
-      publishing()
-      message.error(t('publish_fail'), 2.5)
-      console.log(error);
-    });
-  }
+      .then(function (response) {
+        publishing();
+        message.success(t("publish_success"), 2.5);
+        setApp({
+          ...app,
+          layout,
+        });
+      })
+      .catch(function (error) {
+        publishing();
+        message.error(t("publish_fail"), 2.5);
+        console.log(error);
+      });
+  };
 
   const goToApp = (id) => {
-    const app = apps.filter(app => app.id === id)
-    setApp(app[0])
-  }
+    const app = apps.filter((app) => app.id === id);
+    setApp(app[0]);
+  };
 
   const showAppCode = (id) => {
-    const app = apps.filter(app => app.id === id)
+    const app = apps.filter((app) => app.id === id);
     if (app[0] === undefined) {
-      return
+      return;
     }
     Modal.info({
       content: (
         <div className="code-notification">
-          <p>{t('app_code_title')}</p>
+          <p>{t("app_code_title")}</p>
           <span>{`https://liff.line.me/1657009564-OWeBGjJL/lineapp?code=${app[0].code}`}</span>
         </div>
       ),
       onOk() {},
     });
-  }
+  };
 
   useEffect(() => {
-    getApps()
+    getApps();
   }, [dispatch]);
 
   return (
-    <Layout style={{position: 'relative'}}>
-      {
-        createModal === true &&
+    <Layout style={{ position: "relative" }}>
+      {createModal === true && (
         <AppModal onClose={() => setCreateModal(false)} onCreate={createApp} />
-      }
+      )}
       <Header>
         <img className="header-logo" src={logo} alt="Logo" />
       </Header>
       <Layout>
         <Sider>
-          <Menu mode="inline" onClick={e => {
-            if (e.key.includes('detail')) {
-              const detail = e.key.split('#')
-              showAppCode(detail[0])
-              return
-            }
-            goToApp(e.key)
-          }}>
-            <Menu.ItemGroup title={t('apps')}>
-              {
-                apps.map((app) => (
-                  <SubMenu key={`app_${app.id}`} title={app.name}>
-                    <Menu.Item key={app.id}>{t('layout')}</Menu.Item>
-                    <Menu.Item key={`${app.id}#detail`}>{t('get_app_code')}</Menu.Item>
-                  </SubMenu>
-                ))
+          <Menu
+            mode="inline"
+            onClick={(e) => {
+              if (e.key.includes("detail")) {
+                const detail = e.key.split("#");
+                showAppCode(detail[0]);
+                return;
               }
-              <Skeleton loading={loading} active/>
+              goToApp(e.key);
+            }}
+          >
+            <Menu.ItemGroup title={t("apps")}>
+              {apps.map((app) => (
+                <SubMenu key={`app_${app.id}`} title={app.name}>
+                  <Menu.Item key={app.id}>{t("layout")}</Menu.Item>
+                  <Menu.Item key={`${app.id}#detail`}>
+                    {t("get_app_code")}
+                  </Menu.Item>
+                </SubMenu>
+              ))}
+              <Skeleton loading={loading} active />
             </Menu.ItemGroup>
           </Menu>
-          <Button type="primary" className="home-createapp" onClick={() => setCreateModal(true)} >{t('create_new_app')}</Button>
+          <Button
+            type="primary"
+            className="home-createapp"
+            onClick={() => setCreateModal(true)}
+          >
+            {t("create_new_app")}
+          </Button>
         </Sider>
         <Layout>
           <Content>
-            <AppLayout app={app} onPublish={publishLayout}/>
+            <AppLayout app={app} onPublish={publishLayout} />
           </Content>
         </Layout>
       </Layout>
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import './index.scss';
-import 'AppRoot/App.less';
-import { Input, Form, Button, Spin, message } from 'antd';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { useHistory, useLocation } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { useAuth } from '../../auth/context';
-import axios from 'axios';
-import logo from '../../assets/logo.png';
+import React, { useState } from "react";
+import "./index.scss";
+import "AppRoot/App.less";
+import { Input, Form, Button, Spin, message } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { useHistory, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useAuth } from "../../auth/context";
+import axios from "axios";
+import logo from "../../assets/logo.png";
 
 function Signup() {
   const [logging, setLogging] = useState(false);
@@ -17,73 +17,95 @@ function Signup() {
   const { t } = useTranslation();
 
   const goToSignin = () => {
-    history.replace('/signin');
+    history.replace("/signin");
   };
   const onFinish = (values) => {
-    setLogging(true)
-    axios.post('https://sheltered-garden-95685.herokuapp.com/signup', values)
-    .then(function (response) {
-      setLogging(false)
-      message.success(t('signup_success'), 2.5)
-      history.replace('/signin');
-    })
-    .catch(function (error) {
-      setLogging(false)
-      auth.signout(() => {})
-    });
+    setLogging(true);
+    axios
+      .post("https://sparker-api.onrender.com/signup", values)
+      .then(function (response) {
+        setLogging(false);
+        message.success(t("signup_success"), 2.5);
+        history.replace("/signin");
+      })
+      .catch(function (error) {
+        setLogging(false);
+        auth.signout(() => {});
+      });
   };
 
   const renderSubmitAction = () => {
     if (logging === true) {
-      return (
-        <Spin />
-      )
+      return <Spin />;
     }
     return (
-      <Button htmlType="submit" className="signin-button" size="large" type="primary">{t('signup_action')}</Button>
-    )
-  }
+      <Button
+        htmlType="submit"
+        className="signin-button"
+        size="large"
+        type="primary"
+      >
+        {t("signup_action")}
+      </Button>
+    );
+  };
 
   return (
     <div className="signin-background">
       <img src={logo} alt="Logo" />
       <div className="signin-container">
-        <h1 className="signin-title">{t('signup')}</h1>
+        <h1 className="signin-title">{t("signup")}</h1>
         <Form name="signup" onFinish={onFinish}>
-          <Form.Item name="email" rules={[{ required: true, message: t('email_required') }]}>
-            <Input className="signin-input" size="large" placeholder={t('email')} />
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: t("email_required") }]}
+          >
+            <Input
+              className="signin-input"
+              size="large"
+              placeholder={t("email")}
+            />
           </Form.Item>
-          <Form.Item name="password" rules={[{ required: true, message: t('password_required') }]}>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: t("password_required") }]}
+          >
             <Input.Password
               className="signin-input"
               size="large"
-              placeholder={t('password')}
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              placeholder={t("password")}
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
             />
           </Form.Item>
           <Form.Item
             name="confrim"
-            dependencies={['password']}
+            dependencies={["password"]}
             hasFeedback
-            rules={[{ required: true, message: t('password_confirm') }, ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error(t('password_mismatch')));
-              },
-            })]}>
+            rules={[
+              { required: true, message: t("password_confirm") },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error(t("password_mismatch")));
+                },
+              }),
+            ]}
+          >
             <Input.Password
               className="signin-input"
               size="large"
-              placeholder={t('password_confirm')}
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              placeholder={t("password_confirm")}
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
             />
           </Form.Item>
           <Form.Item>
-            <div>
-              {renderSubmitAction()}
-            </div>
+            <div>{renderSubmitAction()}</div>
             <Button
               className="go-signup"
               type="text"
@@ -91,7 +113,7 @@ function Signup() {
                 goToSignin();
               }}
             >
-              {t('go_to_signin')}
+              {t("go_to_signin")}
             </Button>
           </Form.Item>
         </Form>
